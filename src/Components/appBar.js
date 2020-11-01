6b9e3a76dcb0e9be2ece252921fd263bc236e127
Carl Orge Retzlaff <e.awerg@web.de>
2020-11-01 20:09:19 +0100

    fixed AppBar, ready for ServiceWorking, Themes etc

--- src/Components/appBar.js
+++ src/Components/appBar.js
@@ -5,9 +5,10 @@ import { makeStyles } from '@material-ui/styles'
 const useStyles = makeStyles(theme => 
     ({  root: {    flexGrow: 1  }, 
         flex: {    flex: 1  },
-        //how to align this a little more right?
-        caption: {alignContent: "end", flex:1},
-        toolbarMargin: {minHeight:56} }))
+        title:{letterSpacing:4, fontWeight:800},
+        caption: {alignContent: "end", flex:1, letterSpacing:9,fontWeight:700},
+        toolbarMargin: {minHeight:56},
+     }))
 
 function CustomAppBar(){
     const classes = useStyles()
@@ -15,11 +16,13 @@ function CustomAppBar(){
         <React.Fragment>
             <AppBar position="fixed">
                 <Toolbar>
-                    <Typography variant="h5">
-                        Mensaknecht
+                    <Typography variant="h5" className={classes.title}>
+                        Mensaknecht 
                     </Typography>
+                    &nbsp;
+                    &nbsp;
                     <Typography variant="caption" className={classes.caption}>
-                            Taste the Gainbow
+                       {"    "} Taste the Gainbow
                     </Typography>
                 </Toolbar>
             </AppBar>
